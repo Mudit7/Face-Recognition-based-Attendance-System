5ad972c86df2dfe578c3625bffbcac385d87cd30
prazwal <prazwal.chhabra.007@gmail.com>
2020-04-26 22:18:51 +0000

    added flask wrapper to update attendance query

--- Output_DataBase/updatequery.py
+++ Output_DataBase/updatequery.py
@@ -1,36 +1,51 @@
 import pip
 pip.main(['install','mysql-connector-python-rf'])
 import mysql.connector
+
 from datetime import datetime
 import calendar
-def update_table(l,code):
-    db_connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            passwd="Hardcore@007",
-            database="mydatabase")
-    #print(db_connection)
-    datetime_object = datetime.now()
-    month=datetime_object.month
-    year=datetime_object.year
-    date=datetime_object.day
-    val=str(date)+"_"+str(month)+"_"+str(year)
-    val=str(val)
-    #print('date is:->'+val)
-    val_1=""+val
-    #print(val_1)
-    cursor = db_connection.cursor()
-    for val in l:
-        #sql = "UPDATE student2 SET "+val_1+"='absent' WHERE student_id ="+str(val[2])+" AND subject_code="+str(val[3])
-        #sql = "UPDATE student2 SET "+val_1+"='absent' WHERE student2.student_id ="+str(val[2])+" and student2.subject_code="+str(val[3])
-        #sql="UPDATE student2 SET "+val_1+"='absent' where subject_code='CS471' and student_id=2019201059"
-        sql="UPDATE student5 SET "+val_1+"='present' where subject_code = "+str(code)+" and student_id="+str(val[2])
-        cursor.execute(sql)
-        #cursor.execut
-        db_connection.commit()
-        print('yes')
 
-l=[('vikram',9,2019201059,123),('rish',4,2019201074,123)]
-code=123
-update_table(l,code)
+from flask import Flask, request
+from flask_cors import CORS
 
+app = Flask(__name__)
+cors = CORS(app)
+
+def getDate():
+	datetime_object = datetime.now()
+	month=datetime_object.month
+	year=datetime_object.year
+	date=datetime_object.day
+	val=str(date)+"_"+str(month)+"_"+str(year)
+	val=str(val)
+
+	val_1=""+val
+
+def getDatabaseConnection():
+	db_connection = mysql.connector.connect(
+		host="localhost",
+		user="root",
+		passwd="Hardcore@007",
+		database="mydatabase"
+	)
+	return db_connection, db_connection.cursor()
+
+
+@app.route('/', methods = ['POST'])
+def update_table():
+	if request.method == 'POST':
+		data = request.get_json()
+		subject_code = data['subject_code']
+		present_student_list = data['studentList']
+		
+		db_connection, cursor = getDatabaseConnection()
+
+		date = getDate()
+
+		for student in present_student_list:
+			sql="UPDATE student5 SET "+date+"='present' where subject_code = "+str(subject_code)+" and student_id="+str(student[2])
+			cursor.execute(sql)
+			db_connection.commit()
+
+if __name__ == "__main__":
+	app.run(host='127.0.0.1', port=8081)
