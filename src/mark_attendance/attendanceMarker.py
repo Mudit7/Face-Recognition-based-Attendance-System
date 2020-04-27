ce4d112837f843ba29a499a262723f21a827f434
VikramKes <vikram.keswani@students.iiit.ac.in>
2020-04-27 19:47:50 +0530

    attedancemarkeer

--- src/mark_attendance/attendanceMarker.py
+++ src/mark_attendance/attendanceMarker.py
@@ -1,7 +1,10 @@
 class AttendanceMarker:
     def __init__(self):
+        self.roll_no=[]
         pass
-
-    def mark_present(self, students):
-        print("First one",students[0].getName())
-        return 1
+    def mark_present(self, students,subject_code):
+        for val in range(len(students)):
+            self.roll_no.append(students.getRollNo())    
+        prazwal_method(roll_no,code)
+        
+    
